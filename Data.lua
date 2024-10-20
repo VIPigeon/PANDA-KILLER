d643016901aaa106dccbf5842696b7ab96651a25
Shkobarev <115147184+Shkobarev@users.noreply.github.com>
2024-10-20 21:02:34 +0300

    Добавили тайлы анимаций😸
    1)Бег
    2)Прыжок
    3)Покой

--- Data.lua
+++ Data.lua
@@ -22,6 +22,37 @@ WORLD_HEIGHT = 1088 -- пикселей (= 136 * 8)
 
 data.bad_tile = {}
 data.panda = {}
+data.run = {}
+data.jump = {}
+data.idle = {}
+
+-- Это тайлы для анимаций
+
+data.idle = {
+    --idle_title1 = 409
+    -- Мы добавим еще😎
+}
+
+data.jump = {
+    --jump_tile1 = {396,397}
+    --jump_tile2 = {398}
+    --jump_tile3 = {399}
+    --jump_tile4 = {383}
+    --jump_tile5 = {412,413,428,429}
+    --jump_tile6 = {414,415,430,431}
+    --jump_tile7 = {444,445,460,461}
+    --jump_tile8 = {446,447,462,463}
+    --jump_tile9 = {426,427,442,443}
+}
+
+data.run = {
+    --run_tile1 = {384,385}
+    --run_tile2 = {386,387}
+    --run_tile3 = {388,389,404,405}
+    --run_tile4 = {390,391}
+    --run_tile5 = {392,393}
+    --run_tile6 = {394,395,410,411}
+}
 
 data.bad_tile = {
 	bad_tile1 = 32
